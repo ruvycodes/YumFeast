efe262ced7a45c6ea9ea8537aba7050c36730f57
Dhruv <ruvycodes@gmail.com>
2024-05-16 11:34:52 +0530

    Minor Change

--- src/Components/RestaurantMenu.jsx
+++ src/Components/RestaurantMenu.jsx
@@ -21,39 +21,6 @@ const RestaurantMenu = () => {
       <h1>Ye hai humaara MENUUUU</h1>
       <Notification />
 
-      {/* {resMenu.map((dish)=> (
-                 <p key={dish?.card?.info?.id}>{(dish?.card?.info?.name)}</p>
-            ))} */}
-
-      {/* {resMenu.map((card) => {
-        if (card.card?.card?.categories) {
-          return card.card.card.categories.flatMap((category) =>
-            category?.itemCards
-              ? category.itemCards.map((item) => (
-                <div key={item.card.info.id} className="flex justify-center">
-                  <div className="bg-amber-300 m-2 h-24 w-96 flex justify-center items-center">
-                    <p>
-                      {item.card.info.name}: Rs {item.card.info.price / 100}
-                    </p>
-                  </div>
-                </div>
-              ))
-              : []
-          );
-        } else if (card.card?.card?.itemCards) {
-          return card.card.card.itemCards.map((item) => (
-            <div key={item.card.info.id} className="flex justify-center">
-              <div className="bg-amber-100 m-2 h-24 w-96 flex justify-center items-center">
-                <p>
-                  {item.card.info.name}: Rs {item.card.info.price / 100}
-                </p>
-              </div>
-            </div>
-          ));
-        }
-        return null;
-      })} */}
-
       {
         filteredTitle.map((title, index) => <MenuAccordian key={index} accordianData={title?.card?.card} />)
       }
